e792cab44049f9e5770e30f3f16245fc611f80ff
rickymm3 <rickymm3@gmail.com>
2026-03-20 10:14:35 -0400

    Fix deploy: single DB for Solid subsystems, add migrations, fix home view

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,16 +2,6 @@ class HomeController < ApplicationController
   allow_unauthenticated_access
 
   def index
-    if authenticated?
-      @active_exploration = current_user.explorations.active.first
-
-      # Auto-process finished exploration
-      if @active_exploration && @active_exploration.finishes_at <= Time.current
-        Explorations::ProcessService.new(@active_exploration).call
-        @active_exploration = nil
-      end
-
-      @completed_explorations = current_user.explorations.completed.order(finishes_at: :desc)
-    end
+    redirect_to "/mobile", allow_other_host: false
   end
 end
